1df7a50c10e7d89b60f621d957a966730379a901
James Montalvo <jamesmontalvo3@gmail.com>
2017-01-04 20:31:21 -0600

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+Journal
+=======
+
+Gonna start writing here morningly.
+
+Roadmap
+-------
+
+* Create a "seeds" mechanism for putting randomly adding a seed question to a journal entry. The only current seed question I have is from a PG essay (What you can't say): "Do you have any opinions that you would be reluctant to express in front of a group of your peers?"
+* To-do passing: e.g. make a way for one day to to pass things onto the next day that need to be addressed.
---
+++ now.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+#
+# Creates a journal entry for today
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+YEAR=`date +%Y`
+MONTH=`date +%m`
+DAY=`date +%d`
+TIME=`date +%T`
+TZ=`date +%Z`
+
+YEARDIR="$DIR/$YEAR"
+MONTHDIR="$YEARDIR/$MONTH"
+DAYFILE="$MONTHDIR/$DAY.md"
+
+if [ ! -d "$YEARDIR" ]; then
+	echo "Creating year directory $YEARDIR"
+	mkdir "$YEARDIR"
+fi
+
+if [ ! -d "$MONTHDIR" ]; then
+	echo "Creating month directory $MONTHDIR"
+	mkdir "$MONTHDIR"
+fi
+
+if [ ! -f "$DAYFILE" ]; then
+	echo "Creating day file $DAYFILE"
+
+	echo "$YEAR-$MONTH-$DAY" > "$DAYFILE"
+	echo "==========" >> "$DAYFILE"
+	echo "" >> "$DAYFILE"
+	echo "Sleep: ?" >> "$DAYFILE"
+else
+	echo "" >> "$DAYFILE"
+	echo "" >> "$DAYFILE"
+fi
+
+echo "## $TIME $TZ" >> "$DAYFILE"
+echo "" >> "$DAYFILE"
+
+if [ -z "$EDITOR" ]; then
+	echo "Add an 'editor' variable to your bashrc like:"
+else
+	`"$EDITOR" "$DAYFILE"`
+fi
